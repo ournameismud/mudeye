8a56735d446df45a93e2e1a31802fe895d2dfb9d
Cole Henley <cole@ournameismud.co.uk>
2018-03-07 11:56:17 +0000

    Base variable

--- src/variables/MudEyeVariable.php
+++ src/variables/MudEyeVariable.php
@@ -11,6 +11,10 @@
 namespace ournameismud\mudeye\variables;
 
 use ournameismud\mudeye\MudEye;
+use ournameismud\mudeye\models\Settings;
+use craft\web\UrlManager;
+use craft\web\Request;
+use craft\helpers\Template as TemplateHelper;
 
 use Craft;
 
@@ -21,19 +25,71 @@ use Craft;
  */
 class MudEyeVariable
 {
-    // Public Methods
-    // =========================================================================
+    public $settings,
+        $site,
+        $baseUrl,
+        $devMode = false;
 
-    /**
-     * @param null $optional
-     * @return string
-     */
-    public function exampleVariable($optional = null)
+    public function __construct()
     {
-        $result = "And away we go to the Twig template...";
-        if ($optional) {
-            $result = "I'm feeling optional today...";
-        }
-        return $result;
+        $this->settings = MudEye::$plugin->getSettings();
+        $this->devMode = Craft::$app->getConfig()->getGeneral()->devMode;
+        $this->site = Craft::$app->sites->currentSite;
+        $this->baseUrl = Craft::$app->request->hostInfo;
+        // $this->devMode = (bool) $devMode;
     }
+
+    
+    public function tags($opts)
+    {
+        // abstract to service
+        // move SEO to element?
+        $robots = false;
+        $url = $this->baseUrl;
+        $element = Craft::$app->urlManager->getMatchedElement();
+        $titleArray = array();
+        $separator = ' ' . trim($this->settings['seoSeparator']) . ' ';
+        
+        if ($element && get_class($element) == 'craft\\elements\\Entry') {
+            $title = $element->title;
+            $seoFields = json_decode($element->seoFields);
+            
+            if($seoFields && property_exists($seoFields,'seoRobot') && $seoFields->seoRobot== 1) {
+                $robots = true;
+            }
+            // need to make more reliable than dependence on named field
+            // abstract to settings?
+            if($seoFields && property_exists($seoFields,'seoTitle')) {
+                $titleArray[] = $seoFields->seoTitle;
+            } else {
+                $titleArray[] = $element->title;
+            }
+        } elseif (array_key_exists('title',$opts)) {
+            $titleArray[] = $opts['title'];
+        }
+        
+        $titleArray[] = $this->site->name;
+        $titleString = implode($separator,$titleArray);
+
+        $url .= Craft::$app->request->url;
+
+        $html = "";
+        $html .= "<!-- MudEye SEO: start -->\r\n";
+
+        $html .= "<title>" . $titleString . "</title>\r\n";
+        $html .= "<link rel=\"canonical\" href=\"" . $url . "\" />\r\n";
+
+        if ($this->devMode) {
+            $html .= "<!-- robots: dev mode  -->\r\n";
+        }
+        if ($this->devMode OR $robots) {
+            $html .= "<meta name=\"robots\" content=\"noindex\">\r\n";
+            $html .= "<meta name=\"googlebot\" content=\"noindex\">\r\n";            
+        }
+
+        $html .= "<!-- MudEye SEO: end -->\r\n";
+        
+        return TemplateHelper::raw($html);
+        
+    }    
 }
